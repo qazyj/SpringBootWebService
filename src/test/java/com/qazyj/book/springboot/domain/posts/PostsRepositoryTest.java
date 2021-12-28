6bb63a3a0319eb8ed7e249c321ee4adaede3404f
qazyj <qazyj@naver.com>
2021-12-28 16:42:57 +0900

    [UPDATE] JPA Test

--- src/test/java/com/qazyj/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/qazyj/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -2,12 +2,17 @@ package com.qazyj.book.springboot.domain.posts;
 
 import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostsRepositoryTest {
 
     @Autowired
