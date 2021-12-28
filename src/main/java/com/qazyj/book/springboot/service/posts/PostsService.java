db05caecbb780aaafd163e122a54d3cf4ce8025c
qazyj <qazyj@naver.com>
2021-12-28 18:11:18 +0900

    [ADD] 등록/수정/조회 API

---
+++ src/main/java/com/qazyj/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.qazyj.book.springboot.service.posts;
+
+import com.qazyj.book.springboot.domain.posts.PostsRepository;
+import com.qazyj.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/qazyj/book/springboot/service/temp.java
@@ -0,0 +1,4 @@
+package com.qazyj.book.springboot.service;
+
+public class temp {
+}
