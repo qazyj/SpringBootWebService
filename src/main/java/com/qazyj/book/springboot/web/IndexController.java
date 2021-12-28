3b647097534088b3d539fc17048db40de5137903
qazyj <qazyj@naver.com>
2021-12-29 02:08:23 +0900

    [ADD] Mustache

---
+++ src/main/java/com/qazyj/book/springboot/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.qazyj.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
