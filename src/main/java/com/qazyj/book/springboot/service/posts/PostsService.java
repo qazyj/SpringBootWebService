fb3f522d880f830ff2aeb51d0ac1c830fdcf34a9
qazyj <qazyj@naver.com>
2021-12-28 18:28:52 +0900

    [ADD] 수정/조회

--- src/main/java/com/qazyj/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/qazyj/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.qazyj.book.springboot.service.posts;
 
+import com.qazyj.book.springboot.domain.posts.Posts;
 import com.qazyj.book.springboot.domain.posts.PostsRepository;
+import com.qazyj.book.springboot.web.dto.PostsResponseDto;
 import com.qazyj.book.springboot.web.dto.PostsSaveRequestDto;
+import com.qazyj.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +20,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
