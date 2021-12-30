0cce5cdda58dd4e699133463fa067882c8605e71
qazyj <qazyj@naver.com>
2021-12-30 17:43:46 +0900

    [ADD] @Login

--- src/main/java/com/qazyj/book/springboot/config/temp.java
+++
@@ -1,4 +0,0 @@
-package com.qazyj.book.springboot.config;
-
-public class temp {
-}
--- src/main/java/com/qazyj/book/springboot/web/IndexController.java
+++ src/main/java/com/qazyj/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.qazyj.book.springboot.web;
 
+import com.qazyj.book.springboot.config.auth.LoginUser;
 import com.qazyj.book.springboot.config.auth.dto.SessionUser;
 import com.qazyj.book.springboot.service.posts.PostsService;
 import com.qazyj.book.springboot.web.dto.PostsResponseDto;
@@ -18,10 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
