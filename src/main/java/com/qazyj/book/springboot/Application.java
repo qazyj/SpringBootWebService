f439c1c72fcb7524f0302e0b04ee11ab538ddbd8
qazyj <qazyj@naver.com>
2022-01-05 21:18:25 +0900

    [UPDATE] application.properties

--- src/main/java/com/qazyj/book/springboot/Application.java
+++ src/main/java/com/qazyj/book/springboot/Application.java
@@ -2,7 +2,6 @@ package com.qazyj.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing          // JPA Auditing 활성화
 @SpringBootApplication
