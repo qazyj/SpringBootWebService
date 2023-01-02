91aef8f911b1db4e854c848b9b9057474f44839b
kimyoungjin <qazyj@naver.com>
2023-01-03 00:03:02 +0900

    test

--- src/main/java/com/qazyj/book/springboot/Application.java
+++ src/main/java/com/qazyj/book/springboot/Application.java
@@ -2,6 +2,7 @@ package com.qazyj.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing          // JPA Auditing 활성화
 @SpringBootApplication
@@ -9,7 +10,5 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-        String getVersion = org.springframework.core.SpringVersion.getVersion();
-        System.out.println("version : " + getVersion);
     }
 }
